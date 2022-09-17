550453658378c016c8b03be12ac4f8d277a9b0c6
Loz <nicollaur@gmail.com>
2022-09-17 11:23:33 -0400

    created .singleDiv class and created one 16px by 16px div

---
+++ index.js
@@ -0,0 +1,5 @@
+const containerDiv = document.querySelector(".container");
+
+let singleDiv = document.createElement("div");
+singleDiv.classList.add("single-div");
+containerDiv.appendChild(singleDiv);
