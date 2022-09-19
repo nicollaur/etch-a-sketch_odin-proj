2352dff1c44c1e3d8b39b43147b8d44c1a1d84aa
Loz <nicollaur@gmail.com>
2022-09-19 15:13:27 -0400

    completed

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const containerDiv = document.querySelector(".container");
+const generateBtn = document.querySelector(".generate-btn");
+const input = document.querySelector("#input-el");
 
-let singleDiv = document.createElement("div");
-singleDiv.classList.add("single-div");
-containerDiv.appendChild(singleDiv);
+generateGrid();
+
+generateBtn.addEventListener("click", () => {
+
+    if (input.value < 16 || input.value > 100) {
+        alert("Please enter a number between 16 - 100!");
+    }
+
+    removeAllChildrenNodes(containerDiv);
+    generateGrid();
+
+})
+
+function removeAllChildrenNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function generateGrid() {
+
+    containerDiv.style.gridTemplateColumns = `repeat(${input.value}, 1fr)`;
+    containerDiv.style.gridTemplateRows = `repeat(${input.value}, 1fr)`;
+
+    for (let i = 0; i < (input.value * input.value); i++) {
+        let singleDiv = document.createElement("div")
+        singleDiv.classList.add("single-div");
+        containerDiv.appendChild(singleDiv);
+
+        singleDiv.addEventListener("click", () => {
+            singleDiv.classList.add("single-div-color");
+        })
+    };
+
+}
